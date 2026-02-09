3f15c4fe86f82b393725e12a4221d46da06e3a06
hareeshmlops319 <hareesh.yalamasetty1@gmail.com>
2026-02-09 12:51:40 -0500

    network-security-problem

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+"""
+defining common constant variable for a training pipeline
+"""
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "Networksecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
+
+
+"""
+Data Ingestion releated constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "hareeshyalamasetty"
+DATA_INGESTION_DIR_NAME:str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
